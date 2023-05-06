5c4bdb57b71961098cb2e8784a450b863f157c41
Krisna Pranav <krisna.pranav@gmail.com>
2023-05-06 17:22:35 +0530

    flat: encode[errors]

--- src/flat/src/encode/errors.rs
+++ src/flat/src/encode/errors.rs
@@ -2,8 +2,10 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Buffer is not byte algined")]
+    #[error("Buffer is not byte aligned")]
     BufferNotByteAligned,
     #[error("{0}")]
     Message(String),
+    #[error(transparent)]
+    Custom(#[from] anyhow::Error),
 }
