974fd0aca35ad367a9eb6a9847bb15ee5d9d4295
Krisna Pranav <krisna.pranav@gmail.com>
2023-05-07 10:26:51 +0530

    untyped-plutus-core: machine[discharge]

--- src/untyped-plutus-core/src/machine/discharge.rs
+++ src/untyped-plutus-core/src/machine/discharge.rs
@@ -1,11 +1,22 @@
+/**
+ * @file discharge.rs
+ * @author Krisna Pranav
+ * @version 0.1
+ * @date 2023-05-07
+ *
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ *
+*/
+
 use crate::ast::{NamedDeBruijn, Term};
 use super::value::{Env, Value};
 
 pub(super) fn value_as_term(value: Value) -> Term<NamedDeBruijn> {
     match value {
         Value::Con(x) => Term::Constant(x),
-        Value::Builtin { runtime, fun} => {
+        Value::Builtin { runtime, fun } => {
             let mut term = Term::Builtin(fun);
+
             for _ in 0..runtime.forces {
                 term = term.force();
             }
@@ -16,32 +27,70 @@ pub(super) fn value_as_term(value: Value) -> Term<NamedDeBruijn> {
 
             term
         }
+        Value::Delay(body, env) => with_env(0, env, Term::Delay(body)),
+        Value::Lambda {
+            parameter_name,
+            body,
+            env,
+        } => with_env(
+            0,
+            env,
+            Term::Lambda {
+                parameter_name: NamedDeBruijn {
+                    text: parameter_name.text.clone(),
+                    index: 0.into(),
+                }
+                .into(),
+                body,
+            },
+        ),
     }
 }
 
-fn with_env(lam_cmt: usize, env: Env, term: Term<NamedDeBruijn>) -> Term<NamedDeBruijn> {
+fn with_env(lam_cnt: usize, env: Env, term: Term<NamedDeBruijn>) -> Term<NamedDeBruijn> {
     match term {
         Term::Var(name) => {
             let index: usize = name.index.into();
 
             if lam_cnt >= index {
-                Term::Var(name);
+                Term::Var(name)
             } else {
-                env.get::<usize>(env.len() - (index - lam_cmt))
+                env.get::<usize>(env.len() - (index - lam_cnt))
                     .cloned()
                     .map_or(Term::Var(name), value_as_term)
             }
         }
-
         Term::Lambda {
             parameter_name,
             body,
         } => {
-            let body = with_env(lam_cmt + 1, env, body.as_ref().clone());
+            let body = with_env(lam_cnt + 1, env, body.as_ref().clone());
+
             Term::Lambda {
                 parameter_name,
                 body: body.into(),
             }
         }
+        Term::Apply { function, argument } => {
+            let function = with_env(lam_cnt, env.clone(), function.as_ref().clone());
+            let argument = with_env(lam_cnt, env, argument.as_ref().clone());
+
+            Term::Apply {
+                function: function.into(),
+                argument: argument.into(),
+            }
+        }
+
+        Term::Delay(x) => {
+            let delay = with_env(lam_cnt, env, x.as_ref().clone());
+
+            Term::Delay(delay.into())
+        }
+        Term::Force(x) => {
+            let force = with_env(lam_cnt, env, x.as_ref().clone());
+
+            Term::Force(force.into())
+        }
+        rest => rest,
     }
 }
