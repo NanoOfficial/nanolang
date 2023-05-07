bf76ec738346c6d0339eacc1d6bfeb162f901c9d
Krisna Pranav <krisna.pranav@gmail.com>
2023-05-07 16:08:40 +0530

    untyped-plutus-core: tx[error, script_context, to_plutus_data]

---
+++ src/untyped-plutus-core/src/tx/error.rs
@@ -0,0 +1,70 @@
+/**
+ * @file error.rs
+ * @author Krisna Pranav
+ * @version 0.1
+ * @date 2023-05-06
+ *
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ *
+*/
+
+use crate::machine::{self, cost_model::ExBudget};
+
+#[derive(thiserror::Error, Debug, miette::Diagnostic)]
+pub enum Error {
+    #[error("{0}")]
+    Address(#[from] pallas_addresses::Error),
+    #[error("Only shelley reward addresses can be a part of withdrawals")]
+    BadWithdrawalAddress,
+    #[error("{0}")]
+    FlatDecode(#[from] flat::de::Error),
+    #[error("{0}")]
+    FragmentDecode(#[from] pallas_primitives::Error),
+    #[error("{}\n\n{:#?}\n\n{}", .0, .1, .2.join("\n"))]
+    Machine(machine::Error, ExBudget, Vec<String>),
+    #[error("Native script can't be executed in phase-two")]
+    NativeScriptPhaseTwo,
+    #[error("Can't eval without redeemers")]
+    NoRedeemers,
+    #[error("Mismatch in required redeemers: {} {}", .missing.join(" "), .extra.join(" "))]
+    RequiredRedeemersMismatch {
+        missing: Vec<String>,
+        extra: Vec<String>,
+    },
+    #[error("Extraneous redeemer")]
+    ExtraneousRedeemer,
+    #[error("Resolved Input not found.")]
+    ResolvedInputNotFound,
+    #[error("A key hash cannot be the hash of a script.")]
+    ScriptKeyHash,
+    #[error("PlutusV1 cost model not found.")]
+    V1CostModelNotFound,
+    #[error("PlutusV2 cost model not found.")]
+    V2CostModelNotFound,
+    #[error("Wrong era, Please use Babbage or Alonzo: {0}")]
+    WrongEra(#[from] pallas_codec::minicbor::decode::Error),
+    #[error("Byron address not allowed in Plutus.")]
+    ByronAddressNotAllowed,
+    #[error("Inline datum not allowed in PlutusV1.")]
+    InlineDatumNotAllowed,
+    #[error("Script and input reference not allowed in PlutusV1.")]
+    ScriptAndInputRefNotAllowed,
+    #[error("Address doesn't contain a payment credential.")]
+    NoPaymentCredential,
+    #[error("Missing required datum: {}", hash)]
+    MissingRequiredDatum { hash: String },
+    #[error("Missing required script: {}", hash)]
+    MissingRequiredScript { hash: String },
+    #[error("Missing required inline datum or datum hash in script input.")]
+    MissingRequiredInlineDatumOrHash,
+    #[error("Only stake deregistration and delegation are valid certificate script purposes.")]
+    OnlyStakeDeregAndDelegAllowed,
+    #[error("Redeemer ({}, {}): {}", tag, index, err)]
+    RedeemerError {
+        tag: String,
+        index: u32,
+        err: Box<Error>,
+    },
+    #[error("Failed to apply parameters to Plutus script.")]
+    ApplyParamsError,
+}
---
+++ src/untyped-plutus-core/src/tx/script_context.rs
@@ -0,0 +1,108 @@
+/**
+ * @file script_context.rs
+ * @author Krisna Pranav
+ * @version 0.1
+ * @date 2023-05-06
+ *
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ *
+*/
+
+use pallas_addresses::Address;
+use pallas_codec::utils::KeyValuePairs;
+use pallas_crypto::hash::Hash;
+use pallas_primitives::babbage::{
+    AddrKeyhash, Certificate, Coin, DatumHash, PlutusData, PolicyId, Redeemer, StakeCredential,
+    TransactionInput, TransactionOutput, Value,
+};
+
+use super::to_plutus_data::MintValue;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ResolvedInput {
+    pub input: TransactionInput,
+    pub output: TransactionOutput,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct TxInInfo {
+    pub out_ref: TransactionInput,
+    pub resolved: TxOut,
+}
+#[derive(Debug, PartialEq, Clone)]
+pub enum TxOut {
+    V1(TransactionOutput),
+    V2(TransactionOutput),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum ScriptPurpose {
+    Minting(PolicyId),
+    Spending(TransactionInput),
+    Rewarding(StakeCredential),
+    Certifying(Certificate),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct TxInfoV1 {
+    pub inputs: Vec<TxInInfo>,
+    pub outputs: Vec<TxOut>,
+    pub fee: Value,
+    pub mint: MintValue,
+    pub dcert: Vec<Certificate>,
+    pub wdrl: Vec<(Address, Coin)>,
+    pub valid_range: TimeRange,
+    pub signatories: Vec<AddrKeyhash>,
+    pub data: Vec<(DatumHash, PlutusData)>,
+    pub id: Hash<32>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct TxInfoV2 {
+    pub inputs: Vec<TxInInfo>,
+    pub reference_inputs: Vec<TxInInfo>,
+    pub outputs: Vec<TxOut>,
+    pub fee: Value,
+    pub mint: MintValue,
+    pub dcert: Vec<Certificate>,
+    pub wdrl: KeyValuePairs<Address, Coin>,
+    pub valid_range: TimeRange,
+    pub signatories: Vec<AddrKeyhash>,
+    pub redeemers: KeyValuePairs<ScriptPurpose, Redeemer>,
+    pub data: KeyValuePairs<DatumHash, PlutusData>,
+    pub id: Hash<32>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TxInfo {
+    V1(TxInfoV1),
+    V2(TxInfoV2),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ScriptContext {
+    pub tx_info: TxInfo,
+    pub purpose: ScriptPurpose,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct TimeRange {
+    pub lower_bound: Option<u64>,
+    pub upper_bound: Option<u64>,
+}
+
+pub struct SlotConfig {
+    pub slot_length: u32,
+    pub zero_slot: u64,
+    pub zero_time: u64,
+}
+
+impl Default for SlotConfig {
+    fn default() -> Self {
+        Self {
+            slot_length: 1000,
+            zero_slot: 4492800,
+            zero_time: 1596059091000,
+        }
+    }
+}
