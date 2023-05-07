f5aaec827e2329408c3d6dc50215c890593b3f13
Krisna Pranav <krisna.pranav@gmail.com>
2023-05-07 15:43:06 +0530

    untyped-plutus-core: debruijn[bimap]

---
+++ src/untyped-plutus-core/src/debruijn/bimap.rs
@@ -0,0 +1,42 @@
+/**
+ * @file bimap.rs
+ * @author Krisna Pranav
+ * @version 0.1
+ * @date 2023-05-07
+ *
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ *
+*/
+
+use std::collections::HashMap;
+use crate::ast::Unique;
+use super::Level;
+
+#[derive(Debug)]
+pub struct BiMap {
+    left: HashMap<Unique, Level>,
+    right: HashMap<Level, Unique>
+}
+
+impl BiMap {
+    pub(super) fn new() -> Self {
+        BiMap {
+            right: HashMap::new(),
+            left: HashMap::new(),
+        }
+    }
+
+    pub(super) fn insert(&mut self, unique: Unique, level: Level) {
+        self.left.insert(unique, level);
+        self.right.insert(level, unique);
+    }
+
+    pub(super) fn remove(&mut self, unique: Unique, level: Level) {
+        self.left.remove(&unique);
+        self.right.remove(&level);
+    }
+
+    pub(super) fn get(&self, unique: &Unique) -> Option<Level> {
+        self.left.get(unique)
+    }
+}
